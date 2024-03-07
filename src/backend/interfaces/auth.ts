1ac57a63f7698d803c3f759d4b09dadccd3326fc
unknown <pedro_gonsalves@hotmail.com>
2024-03-06 22:53:39 -0300

    refac: separating interfaces per file

---
+++ src/backend/interfaces/auth.ts
@@ -0,0 +1,9 @@
+export interface Token {
+    token:string;
+    expiration_time:string;
+}
+
+export interface RefreshToken {
+    token:string;
+    expiration_time:string;
+}
--- src/backend/interfaces/interfaces.ts
+++ src/backend/interfaces/interfaces.ts
@@ -3,8 +3,8 @@ export interface Article {
     revision: string;
     name: string;
     description: string;
-    text_pt: string;
-    text_en: string;
+    text_pt: string | null;
+    text_en: string | null;
     tags: string[];
     published:boolean;
     updated_at: string;
