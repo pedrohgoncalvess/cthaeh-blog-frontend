0195be9e2139f0c62466f9430cdde3ae035eb1e1
unknown <pedro_gonsalves@hotmail.com>
2024-03-06 22:54:33 -0300

    fix: change auth method

--- src/requests/auth.ts
+++ src/requests/auth.ts
@@ -1,16 +1,26 @@
-import type {
-    Articles
-} from '../backend/interfaces.ts'
 import {apiHost} from "./main.js";
+import type {RefreshToken, Token} from "../backend/interfaces/auth.js";
 
 
-
-export async function authUser(username:string, password:string): Promise<Boolean> {
+export async function authUser(username:string, password:string): Promise<[Token | null, RefreshToken | null]> {
 
     const request = await fetch(`${apiHost}/auth`,{
-        method: 'GET',
-        headers: {'Authorization':`Basic ${btoa(`${username}:${password}`)}`}
+        method: 'POST',
+        body: JSON.stringify(
+            {
+                "username":username,
+                "password":password
+            }
+        ),
+        headers: {"Content-Type": "application/json"}
     });
 
-    return request.status == 200;
+    if (request.status == 200) {
+        const jsonResponse = await request.json()
+        const token = jsonResponse['auth_token'] as Token
+        const refreshToken = jsonResponse['refresh_token'] as RefreshToken
+        return [token, refreshToken]
+    }
+
+    return [null, null]
 }
